56fec0fde2bd295b10216c16e73a38ac8e5edfc0
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-03-04 15:42:26 +0700

    benerin harga jual dan beli Praktikum 3 – Membuat file seeder

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -10,16 +10,16 @@ class BarangSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['barang_id' => 1, 'kategori_id' => 1, 'barang_kode' => 'BRG001', 'barang_nama' => 'Laptop', 'harga' => 5000000],
-            ['barang_id' => 2, 'kategori_id' => 1, 'barang_kode' => 'BRG002', 'barang_nama' => 'Smartphone', 'harga' => 3000000],
-            ['barang_id' => 3, 'kategori_id' => 2, 'barang_kode' => 'BRG003', 'barang_nama' => 'Kaos', 'harga' => 100000],
-            ['barang_id' => 4, 'kategori_id' => 2, 'barang_kode' => 'BRG004', 'barang_nama' => 'Celana Jeans', 'harga' => 250000],
-            ['barang_id' => 5, 'kategori_id' => 3, 'barang_kode' => 'BRG005', 'barang_nama' => 'Roti', 'harga' => 50000],
-            ['barang_id' => 6, 'kategori_id' => 3, 'barang_kode' => 'BRG006', 'barang_nama' => 'Susu', 'harga' => 20000],
-            ['barang_id' => 7, 'kategori_id' => 4, 'barang_kode' => 'BRG007', 'barang_nama' => 'Obeng', 'harga' => 15000],
-            ['barang_id' => 8, 'kategori_id' => 4, 'barang_kode' => 'BRG008', 'barang_nama' => 'Tang', 'harga' => 30000],
-            ['barang_id' => 9, 'kategori_id' => 5, 'barang_kode' => 'BRG009', 'barang_nama' => 'Novel', 'harga' => 75000],
-            ['barang_id' => 10, 'kategori_id' => 5, 'barang_kode' => 'BRG010', 'barang_nama' => 'Komik', 'harga' => 50000],
+            ['barang_id' => 1, 'kategori_id' => 1, 'barang_kode' => 'BRG001', 'barang_nama' => 'Laptop', 'harga_beli' => 5000000, 'harga_jual' => 55000000],
+            ['barang_id' => 2, 'kategori_id' => 1, 'barang_kode' => 'BRG002', 'barang_nama' => 'Smartphone', 'harga_beli' => 3000000, 'harga_jual' => 3500000],
+            ['barang_id' => 3, 'kategori_id' => 2, 'barang_kode' => 'BRG003', 'barang_nama' => 'Kaos', 'harga_beli' => 100000, 'harga_jual' => 150000],
+            ['barang_id' => 4, 'kategori_id' => 2, 'barang_kode' => 'BRG004', 'barang_nama' => 'Celana Jeans', 'harga_beli' => 250000, 'harga_jual' => 300000],
+            ['barang_id' => 5, 'kategori_id' => 3, 'barang_kode' => 'BRG005', 'barang_nama' => 'Roti', 'harga_beli' => 50000, 'harga_jual' => 55000],
+            ['barang_id' => 6, 'kategori_id' => 3, 'barang_kode' => 'BRG006', 'barang_nama' => 'Susu', 'harga_beli' => 200000, 'harga_jual' => 250000],
+            ['barang_id' => 7, 'kategori_id' => 4, 'barang_kode' => 'BRG007', 'barang_nama' => 'Obeng', 'harga_beli' => 15000, 'harga_jual' => 160000],
+            ['barang_id' => 8, 'kategori_id' => 4, 'barang_kode' => 'BRG008', 'barang_nama' => 'Tang', 'harga_beli' => 30000, 'harga_jual' => 35000],
+            ['barang_id' => 9, 'kategori_id' => 5, 'barang_kode' => 'BRG009', 'barang_nama' => 'Novel', 'harga_beli' => 75000, 'harga_jual' => 80000],
+            ['barang_id' => 10, 'kategori_id' => 5, 'barang_kode' => 'BRG010', 'barang_nama' => 'Komik', 'harga_beli' => 50000, 'harga_jual' => 55000],
         ];
 
         DB::table('m_barang')->insert($data);
