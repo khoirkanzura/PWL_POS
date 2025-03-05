acda59e6e22ce048f3c23a5cbc55452601cbf733
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-03-05 22:08:06 +0700

    Praktikum 2.1 – Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,25 +12,36 @@ class UserController extends Controller
     public function index()
     {
         // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
 
-            // 'nama' => 'Pelanggan Pertama'
-            // 'username' => 'customer-1',
-            // 'nama' => 'Pelanggan',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 4
-        ];
-        UserModel::create($data); // tambahkan data ke tabel m_user
+        //     // 'nama' => 'Pelanggan Pertama'
+        //     // 'username' => 'customer-1',
+        //     // 'nama' => 'Pelanggan',
+        //     // 'password' => Hash::make('12345'),
+        //     // 'level_id' => 4
+        // ];
+        // UserModel::create($data); // tambahkan data ke tabel m_user
 
-        // UserModel::where('username', 'customer-1') ->update($data);
-        // UserModel::insert($data);// tambahkan data ke tabel m_user
+        // // UserModel::where('username', 'customer-1') ->update($data);
+        // // UserModel::insert($data);// tambahkan data ke tabel m_user
 
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // // coba akses model UserModel
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1); // ambil data user dengan id 1
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstwhere('level_id', 1)->first(); // ambil data user dengan id 1
+        // return view('user', ['data' => $user]);
+        
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+                abort(404);
+        }); 
         return view('user', ['data' => $user]);
     }
 }
