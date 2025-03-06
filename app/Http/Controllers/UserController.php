e67fc9851b667af6f2cceee25d344b81f11bd40b
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-03-06 15:58:38 +0700

    Praktikum 2.5 Langkah 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -58,20 +58,33 @@ class UserController extends Controller
        
        
         
-        $user = UserModel::firstOrNew(
-            [
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ],
-    );
         
-        $user->save();
+                $user = UserModel::create([
+                    'username' => 'manager55',
+                    'nama' => 'Manager55',
+                    'password' => Hash::make('12345'),
+                    'level_id' => 2
+                ]);
+                
+                $user->username = 'manager56';
+                
+                // Mengecek perubahan atribut
+                $user->isDirty('username'); // true
+                $user->isDirty('nama'); // false
+                $user->isDirty(['nama', 'username']); // true
+                
+                $user->isClean('username'); // false
+                $user->isClean('nama'); // true
+                $user->isClean(['nama', 'username']); // false
+                
+                $user->save();
+                
+                $user->isDirty(); // false
+                $user->isClean(); // true
+                dd($user->isDirty());
+            }
+        }
         
-        return view('user', ['data' => $user]);
-    }
-}
 
         
     
