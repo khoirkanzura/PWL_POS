2c6affd2d54a4618ee138e3750734dafd037e2f1
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-03-08 12:41:59 +0700

    Praktikum 2.7 – Relationships

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,75 @@
 
 namespace App\Http\Controllers;
 
-// use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    
     public function index()
     {
+        $user = UserModel::with('level')->get(); // Mengambil semua data dari tabel users
+        return view('user', ['data' => $user]);
+
+        // $user = UserModel::with('level')->get(); // Mengambil semua data dari tabel users
+        // dd($user);
+        // $user = UserModel::all(); // Mengambil semua data dari tabel users
+        // return view('user', ['data' => $user]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        // Simpan data ke database
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+    
+
+        public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+    }
+
+    
+    
+}
+ 
         // tambah data user dengan Eloquent Model
         // $data = [
         //     'level_id' => 2,
@@ -30,9 +90,7 @@ class UserController extends Controller
         // UserModel::insert($data);// tambahkan data ke tabel m_user
 
         // // coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
-
+        
         // $user = UserModel::find(1); // ambil data user dengan id 1
         // return view('user', ['data' => $user]);
 
@@ -59,25 +117,27 @@ class UserController extends Controller
        
         
         
-                $user = UserModel::create([
-                    'username' => 'manager11',
-                    'nama' => 'Manager11',
-                    'password' => Hash::make('12345'),
-                    'level_id' => 2
-                ]);
+                // $user = UserModel::create([
+                //     'username' => 'manager11',
+                //     'nama' => 'Manager11',
+                //     'password' => Hash::make('12345'),
+                //     'level_id' => 2
+                // ]);
                 
-                $user->username = 'manager12';
-                $user->save();
+                // $user->username = 'manager12';
+                // $user->save();
 
-                // Mengecek perubahan 
-                $user->wasChanged(); // true
-                $user->wasChanged('username'); // true
-                $user->wasChanged(['username', 'level_id']); // true
-                $user->wasChanged('nama'); // false
-                $user->wasChanged(['nama', 'username']); // true
-                dd($user->wasChanged(['nama', 'username']));
-            }
-        }
+                // // Mengecek perubahan 
+                // $user->wasChanged(); // true
+                // $user->wasChanged('username'); // true
+                // $user->wasChanged(['username', 'level_id']); // true
+                // $user->wasChanged('nama'); // false
+                // $user->wasChanged(['nama', 'username']); // true
+                // dd($user->wasChanged(['nama', 'username']));
+
+               
+            
+        
         
 
         
--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,54 @@ use Illuminate\Support\Facades\Route;
 
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
-// Route::get('/usermodel', [UserModel::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah', [UserController::class, 'tambah_simpan'])->name('user.tambah');
+Route::get('/user/edit/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah');
+Route::delete('/user/hapus/{id}', [UserController::class, 'hapus'])->name('user.hapus');
+
+
+
+// *
+// |--------------------------------------------------------------------------
+// | Web Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register web routes for your application. These
+// | routes are loaded by the RouteServiceProvider and all of them will
+// | be assigned to the "web" middleware group. Make something great!
+// |
+// */
+
+// <?php
+
+// use App\Http\Controllers\KategoriController;
+// use App\Http\Controllers\LevelController;
+// use App\Http\Controllers\UserController;
+// use Illuminate\Support\Facades\Route;
+// // use App\Models\UserModel;
+
+
+
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah', [UserController::class, 'tambah_simpan'])->name('user.tambah');
+// Route::get('/user/edit/{id}', [UserController::class, 'ubah']);
