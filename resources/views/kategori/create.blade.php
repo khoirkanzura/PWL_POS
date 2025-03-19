b5b3e55f2198ad9c1f3156a830f3e3bf2264f34a
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-03-19 14:41:51 +0700

    B.	VALIDASI PADA SERVER

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -1,11 +1,65 @@
 @extends('layout.app')
+
 {{-- Customize layout sections --}}
+
 @section('subtitle', 'Kategori')
+@section('content_header_title', 'Kategori')
+@section('content_header_subtitle', 'Create')
+{{-- Content body: main page content --}}
+@section('content')
+    <div class="container">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Buat Kategori Baru</h3>
+            </div>
+            <form method="POST" action="../kategori">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="kodeKategori">Kode Kategori</label>
+                        <input id="kategori_kode" 
+                               type="text" 
+                               name="kategori_kode"
+                               class="form-control @error('kategori_kode') is-invalid @enderror" placeholder="Kode Kategori">
+                        @error('kategori_kode')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="namaKategori">Nama Kategori</label>
+                        <input id="kategori_nama" type="text" name="kategori_nama"
+                            class="form-control @error('kategori_nama') is-invalid @enderror"
+                            placeholder="Nama Kategori">
+                        @error('kategori_nama')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    {{-- @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        <ul>
+                    </div>
+                    @endif --}}
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+            <div style="margin-bottom: 20px;"></div> {{-- Tambahkan ruang kosong --}}
+        </div>
+    </div>
+@endsection
+
+{{-- @extends('layout.app')
+{{-- Customize layout sections --}}
+{{-- @section('subtitle', 'Kategori')
 @section('header_title', 'Kategori')
 @section('header_subtitle', 'Create')
 
 {{-- Content body: main page content --}}
-@section('content')
+{{-- @section('content')
 <div class="container">
     <div class="card card-primary">
         <div class="card-header">
@@ -28,5 +82,5 @@
             </div>
         </form>
     </div>
-</div>
-@endsection
+</div> --}}
+{{-- @endsection  --}}
