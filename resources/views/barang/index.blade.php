fa08afbb2be5364a149c3eafb310d7af7525af1e
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-04-16 10:55:56 +0700

    Praktikum 3 – Implementasi Export PDF di Laravel dengan dompdf

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -7,6 +7,7 @@
             <div class="card-tools">
                 <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-sm btn-info">Import Barang</button> 
                 <a href="{{ url('/barang/export_excel') }}" class="btn btn-sm btn-primary"><i class="fa fa-file-excel"></i> Export Barang</a>
+                <a href="{{ url('/barang/export_pdf') }}" class="btn btn-sm btn-warning"><i class="fa fa-file-pdf"></i> Export Barang</a>
                 <button onclick="modalAction('{{ url('barang/create_ajax') }}')" class="btn btn-sm btn-success">
                     Tambah Ajax
                 </button>
