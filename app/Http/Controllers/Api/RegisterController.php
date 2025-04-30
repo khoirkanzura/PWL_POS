24d9f2bd92de8968fe040405b42c8f11afed5edd
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-04-30 20:31:49 +0700

    Praktikum 2 – Membuat RESTful API Login

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -9,39 +9,39 @@ use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    public function __invoke(Request $request){
-        //set Validation
+    public function __invoke(Request $request)
+    {
+        //set validation
         $validator = Validator::make($request->all(), [
             'username' => 'required',
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
-            'level_id' => 'required'
         ]);
-        
-        //if validations fails
-        if($validator->fails()) {
+
+        //if validation fails
+        if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        // create user
+        //create user
         $user = UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
-            'level_id' => $request->level_id
+            'level_id' => $request->level_id,
         ]);
 
-        // return response JSON user is created
-        if ($user) {
+        //return response JSON user is created
+        if($user) {
             return response()->json([
-                'success' => true,
+                'message' => 'User created successfully',
                 'user' => $user,
             ], 201);
-        }
 
-        // return JSON process insert failed
-        return response()->json([
-            'success' => false,
-        ], 409);
+            //return JSON process insert failed
+            return response()->json([
+                'message' => 'User creation failed',
+            ], 409);
+        }
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,50 +2,57 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject; // implementasi JWTSubject
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
 class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
-    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model
-    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
-
-    protected $fillable = ['level_id','username','nama','password', 'picture_path'];
-
-    protected $hidden = ['password']; // jangan ditampilkan saat di select
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
-
-    public function level(): BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id','level_id');
-    }
-
-    // Mendapatkan nama role
-    public function getRoleName(): string{
-        return $this->level->level_nama;
-    }
-
-    // cek apakah user memiliki role tertentu
-    public function hasRole($role): bool {
-        return $this->level->level_kode == $role;
-    }
-
-    // mendapatkan kode role
-    public function getRole(){
-        return $this->level->level_kode;
-    }
-
-    public function getJWTIdentifier(){
+    /**
+     * Get the identifier that will be stored in the JWT.
+     */
+    public function getJWTIdentifier(): mixed
+    {
         return $this->getKey();
     }
 
-    public function getJWTCustomClaims(){
-        return[];
+    /**
+     * Return a key-value array, containing any custom claims to be added to the JWT.
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at', 'foto'];
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
+    /**
+     * Relasi ke tabel level
+     */
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    
+    public function getRoleName(): string
+    {
+        return $this->level->level_name;
+    }
+
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+
+    public function getRole() 
+    {
+        return $this->level->level_kode;
+    }
 }
