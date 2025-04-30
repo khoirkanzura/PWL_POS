c14de181674ef001c7e2e54c9e154551b5309b82
Khoir Karol Nurzuraidah <khoirkarolnurzuraidah23@gmail.com>
2025-04-30 23:05:52 +0700

    TUGAS - Implementasikan CRUD API pada tabel lainnya yaitu tabel m_user, m_kategori, dan m_barang

--- routes/api.php
+++ routes/api.php
@@ -33,20 +33,20 @@ Route::get('levels/{level}', [LevelController::class, 'show']);
 Route::put('levels/{level}', [LevelController::class, 'update']);
 Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
-// Route::get('users', [UserController::class, 'index']);
-// Route::post('users', [UserController::class, 'store']);
-// Route::get('users/{user}', [UserController::class, 'show']);
-// Route::put('users/{user}', [UserController::class, 'update']);
-// Route::delete('users/{user}', [UserController::class, 'destroy']);
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
 
-// Route::get('kategoris', [KategoriController::class, 'index']); 
-//  Route::post('kategoris', [KategoriController::class, 'store']); 
-//  Route::get('kategoris/{kategori}', [KategoriController::class, 'show']); 
-//  Route::put('kategoris/{kategori}', [KategoriController::class, 'update']); 
-//  Route::delete('kategoris/{kategori}', [KategoriController::class, 'destroy']);
+Route::get('kategoris', [KategoriController::class, 'index']); 
+ Route::post('kategoris', [KategoriController::class, 'store']); 
+ Route::get('kategoris/{kategori}', [KategoriController::class, 'show']); 
+ Route::put('kategoris/{kategori}', [KategoriController::class, 'update']); 
+ Route::delete('kategoris/{kategori}', [KategoriController::class, 'destroy']);
 
-//  Route::get('barangs', [BarangController::class, 'index']);
-//  Route::post('barangs', [BarangController::class, 'store']);
-//  Route::get('barangs/{barang}', [BarangController::class, 'show']);
-//  Route::put('barangs/{barang}', [BarangController::class, 'update']);
-//  Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
+ Route::get('barangs', [BarangController::class, 'index']);
+ Route::post('barangs', [BarangController::class, 'store']);
+ Route::get('barangs/{barang}', [BarangController::class, 'show']);
+ Route::put('barangs/{barang}', [BarangController::class, 'update']);
+ Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
